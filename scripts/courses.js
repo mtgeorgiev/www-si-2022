39cd1b6fc4a1def5adf285e84faa13861b137793
Misho G <mihail.georgiev@falcon.io>
2022-04-04 19:26:57 +0300

    the courses page is now a plain html page

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,9 +1,1 @@
 
-const myMouseoverHandler = event => {
-    console.log(event);
-}
-
-
-const goCourseElement = document.getElementById('1');
-goCourseElement.querySelector('a')
-               .addEventListener('mouseover', myMouseoverHandler);
